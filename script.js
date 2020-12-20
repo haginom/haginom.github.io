3bde98159d0a9bae705bcdf657020dc5c1ce33f3
Saki Reid <saki.reid@gmail.com>
2020-12-20 20:40:06 +0100

    first two pages

---
+++ script.js
@@ -0,0 +1,14 @@
+const other = document.getElementById('other')
+other.style.display = "none";
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    changeSection();
+  });
+
+  function changeSection(){
+    const intro = document.getElementById('first')
+    intro.style.display = "none";
+    const other = document.getElementById('other')
+    other.style.display = "block";
+  }
