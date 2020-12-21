7db59eae2e4946cc092b2e594b9605097b58aac8
Saki Reid <saki.reid@gmail.com>
2020-12-21 17:17:24 +0100

    added functionality to toggle between pages

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-const other = document.getElementById('other')
-other.style.display = "none";
+const intro = document.getElementById('first')
+const second = document.getElementById('second')
+const third = document.getElementById('third')
+const fourth = document.getElementById('fourth')
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
+const sections = [intro, second, third, fourth]
+
+intro.style.display = "flex";
+second.style.display= "none";
+third.style.display= "none";
+fourth.style.display = "none";
+
+const btnOne = document.querySelector('#btn-one');
+const btnTwo = document.querySelector('#btn-two');
+const btnThree = document.querySelector('#btn-three');
+const btnFour = document.querySelector('#btn-four')
+
+btnOne.addEventListener('click', () => {
     changeSection();
   });
+btnTwo.addEventListener('click', () => {
+    changeSection();
+});  
+btnThree.addEventListener('click', () => {
+    changeSection();
+});  
+btnFour.addEventListener('click', () => {
+    changeSection();
+});  
 
-  function changeSection(){
-    const intro = document.getElementById('first')
-    intro.style.display = "none";
-    const other = document.getElementById('other')
-    other.style.display = "block";
-  }
+function changeSection(){
+    for (let i=0; i<sections.length; i++){
+        if (i < sections.length-1){
+            if (sections[i].style.display != "none"){
+                sections[i+1].style.display = "block";
+                sections[i].style.display = "none";
+                return;
+            }
+        } else {
+            console.log(i)
+            sections[i].style.display = "none";
+            sections[0].style.display = "flex";
+            
+            return
+        }
+    }
+  };
 
-
-
-for (let i=0; i<4; i++){
-    
-}
